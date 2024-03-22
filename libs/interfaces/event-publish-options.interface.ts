96c229b521bba022128eea69c6a2e999348ac424
choewy <choewy32@gmail.com>
2024-03-22 19:59:17 +0900

    feature/interfaces

---
+++ libs/interfaces/event-publish-options.interface.ts
@@ -0,0 +1,4 @@
+export interface EventPublishOptions {
+  args?: any[];
+  throwError?: boolean;
+}
---
+++ libs/interfaces/index.ts
@@ -0,0 +1,2 @@
+export * from './event-module-options.interface';
+export * from './event-commit-options.interface';
