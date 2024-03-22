c7be412fb531167c3d09dcc6af1142bfea2de481
choewy <choewy32@gmail.com>
2024-03-22 19:59:47 +0900

    feature/implements

---
+++ libs/implements/event-handle-result-reducer.ts
@@ -0,0 +1,76 @@
+import { EventHandlerResultReducerGetValueOptions } from './types';
+import { OnEventHandlerReturnType } from '../decorators';
+
+export class EventHandleResultReducer {
+  constructor(private readonly results: OnEventHandlerReturnType[]) {}
+
+  getValues(opts?: EventHandlerResultReducerGetValueOptions) {
+    const values: any[] = [];
+
+    for (const result of this.results) {
+      if (result.value === undefined) {
+        if (opts?.includeUndefined === true) {
+          values.push(result.value);
+        }
+
+        continue;
+      }
+
+      if (result.value === null) {
+        if (opts?.includeNull === true) {
+          values.push(result.value);
+        }
+
+        continue;
+      }
+
+      values.push(result.value);
+    }
+
+    return values;
+  }
+
+  getFirstValue(opts?: EventHandlerResultReducerGetValueOptions) {
+    return (
+      this.results.find((result) => {
+        if (result.error) {
+          return false;
+        }
+
+        if (result.value === undefined) {
+          return opts?.includeUndefined;
+        }
+
+        if (result.value === null) {
+          return opts?.includeNull;
+        }
+
+        return true;
+      })?.value ?? null
+    );
+  }
+
+  getErrors() {
+    const errors: Error[] = [];
+
+    for (const result of this.results) {
+      if (result.error instanceof Error) {
+        errors.push(result.error);
+      }
+    }
+
+    return errors;
+  }
+
+  getFirstError() {
+    return this.results.find((result) => result.error instanceof Error)?.error ?? null;
+  }
+
+  getValueByClassName(classname: string) {
+    return this.results.find((result) => result.context.getClass()?.name === classname)?.value ?? null;
+  }
+
+  getValueByHandlerName(handlername: string) {
+    return this.results.find((result) => result.context.getHandler()?.name === handlername)?.value ?? null;
+  }
+}
---
+++ libs/implements/types.ts
@@ -0,0 +1,4 @@
+export type EventHandlerResultReducerGetValueOptions = {
+  includeNull?: boolean;
+  includeUndefined?: boolean;
+};
